f5fa0b6805c6ff54c1c68bfe45e48001c011c11d
dard <m_lehmeier@gmx.de>
2021-10-16 19:54:12 +0200

    Menu richtig sortiert

--- TomcatDemo/src/main/java/com/prodato/norma/demo/tomcat/utils/MenuLoader.java
+++ TomcatDemo/src/main/java/com/prodato/norma/demo/tomcat/utils/MenuLoader.java
@@ -4,7 +4,10 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 import org.apache.commons.io.FileUtils;
 
@@ -24,13 +27,20 @@ public class MenuLoader {
     public MenuLoader(final File menuFolder) {
         this.menuFolder = menuFolder;
         final Collection<File> xmlFiles = FileUtils.listFiles(menuFolder, XML_EXTENSIONS, false);
+        final Set<File> xmlFilesSorted = new TreeSet<>(new Comparator<File>() {
+            @Override
+            public int compare(final File f1, final File f2) {
+                return f1.getName().compareTo(f2.getName());
+            }
+        });
+        xmlFilesSorted.addAll(xmlFiles);
 
         final List<Menu> menus = new ArrayList<>();
         try {
             final JAXBContext jaxbContext = JAXBContext.newInstance(Menu.class);
             final Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
 
-            for (final File file : xmlFiles) {
+            for (final File file : xmlFilesSorted) {
                 final Menu menu = (Menu) jaxbUnmarshaller.unmarshal(file);
                 menus.add(menu);
             }
