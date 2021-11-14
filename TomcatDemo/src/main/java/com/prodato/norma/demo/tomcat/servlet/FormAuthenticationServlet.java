93d07b070d0ec93a34b518dc7f562bd9bcd5f587
dard <m_lehmeier@gmx.de>
2021-11-14 19:33:40 +0100

    [auth] FORM Authentifizierung

---
+++ TomcatDemo/src/main/java/com/prodato/norma/demo/tomcat/servlet/FormAuthenticationServlet.java
@@ -0,0 +1,48 @@
+package com.prodato.norma.demo.tomcat.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+/**
+ * Einfaches Servlet für FORM Authentication.
+ * Im Gegensatz zum Basic-Beispiel wird die Gültigkeit unter reinem HTML mit getestet.
+ * Ursprung des Demos: http://www.avajava.com/tutorials/lessons/how-do-i-use-form-authentication-with-tomcat.html
+ *
+ * tomcat-users.xml muss folgende Zeilen beinhalten:
+ *
+ * <role rolename="tomcat"/>
+ * <user username="tomcat" password="tomcat" roles="tomcat"/>
+ * <user username="myname" password="mypassword" roles="tomcat"/>
+ * <user username="test" password="test"/>
+ *
+ * Eine projektspezifische Definition der Rollen ist nicht vorgesehen.
+ *
+ */
+public class FormAuthenticationServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(final HttpServletRequest request, final HttpServletResponse response) throws IOException {
+        // Weiterleiten auf eine HTML-Seite
+        final RequestDispatcher rd = request.getRequestDispatcher("html/webcontent/hello.html");
+        try {
+            rd.forward(request, response);
+        } catch (final ServletException e) {
+
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            response.setContentType("text/html");
+            final PrintWriter out = response.getWriter();
+            out.println("<p>Ein Fehler ist aufgreten. Logs anschauen!");
+        }
+    }
+
+}
